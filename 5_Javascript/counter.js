2cd5c1d37e90422f900e9faaad35916487f751e7
dauviet92 <dauxuanviet@gmail.com>
2024-02-25 20:42:43 +0100

    week5

---
+++ 5_Javascript/counter.js
@@ -0,0 +1,11 @@
+
+let counter = 0;
+            
+function count() {
+    counter++;
+    document.querySelector('h2').innerHTML = counter;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('button').onclick = count;
+});
